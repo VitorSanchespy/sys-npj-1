643c4c31d46eb9cf9f67ade360e00f4e038e1d4f
Vitor Hugo Braga Sanches <101882451+VitorSanchespy@users.noreply.github.com>
2025-07-03 09:24:28 -0300

    Update Users
    Atualização do usuario por QueryString

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,5 @@
 const db = require('../config/db');
+// TODOS OS USUARIOS
 exports.allUsers = (req, res) => {
   db.query('SELECT * FROM usuarios', (error, results) => {
     if (error) {
@@ -8,4 +9,19 @@ exports.allUsers = (req, res) => {
     console.log(results);
     res.json(results);
   });
+};
+
+// UPDATE USERS
+exports.updateUser = (req, res) => {
+  const id = req.params.id;
+  const dadosAtualizados = req.body;
+
+  db.query(
+    'UPDATE usuarios SET nome=?, email=?, role_id=? WHERE id=?',
+    [dadosAtualizados.nome, dadosAtualizados.email, dadosAtualizados.role_id, id],
+    (erro, resultado) => {
+      if (erro) return res.status(500).json({ erro });
+      res.json({ mensagem: 'Perfil atualizado com sucesso' });
+    }
+  );
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,7 +3,7 @@ const users = require('../controllers/users');
 //const authController = require('../controllers/authController');
 const allRoles = require('../controllers/rolesController');
 const rota = Router()
-
+rota.put('/users/:id', users.updateUser)
 rota.get('/users', users.allUsers)
 rota.get('/roles', allRoles.listarRoles)
 rota.get('/', (req, res) => {
